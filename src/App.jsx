4171ab2e61236e3871fae9e2819ad607824ffbbd
BeheadedBadger <ZarradeRuijter@gmail.com>
2024-05-21 17:02:41 +0200

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+  return (<>
+  <h1>Handbags & Purses</h1>
+  <nav>
+    <button>to the collection</button>
+    <button>shop all bags</button>
+    <button disabled="true">pre-orders</button>
+  </nav>
+  </>)
 }
 
 export default App
